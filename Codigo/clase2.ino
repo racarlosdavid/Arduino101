6976c944f361501309c9653873dd276d99944c02
racarlosdavid <2111724640101@ingenieria.usac.edu.gt>
2022-09-24 15:51:45 -0600

    Âdocs: add class 2 README.md

---
+++ Codigo/clase2.ino
@@ -0,0 +1,26 @@
+int resultado = 0;
+
+void setup() {
+  Serial.begin(9600);    
+}
+
+void loop() {
+  metodoSumar(10,20);
+  resultado = FuncionSumar(100,5000);
+  Serial.println(resultado);
+}
+
+// Metodo: NO devuelve el resultado cuando se ejecuta, el tipo de dato del metodo es void
+void metodoSumar(int num1,int num2){
+  resultado = num1 + num2; 
+  Serial.println(resultado);   
+}
+
+/*
+Funcion: devuelve el resultado cuando se ejecuta, el tipo de dato es el tipo de dato del valor que se retorna
+*/
+int FuncionSumar(int num1,int num2){
+  return num1 + num2;   
+}
+
+
